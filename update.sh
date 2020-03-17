7d49cc4c34dbd3a66828a5ab949cae2c739e5dc9
violettwasc <violettwasc@gmail.com>
2020-03-17 10:37:13 +0000

    Update by ShellScript
    Centos7(yum),Ubuntu and more(apt-get)
    And don't forget change permission before uses.

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#If you using Centos7, delete #
+#sudo yum update
+
+#If you using Ubuntu, delete #
+#sudo apt-get update
+#sudo apt-get upgrade
+
+#Write update date in file named update_history.txt (delete #)
+#date >>update_history.txt
+
+#if you want to use this shellscript, it needs change permission.
+#Like,
+#chmod +x update.sh
